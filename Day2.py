6aa3644bbf56496e30d1a1fe09a5dce67dcd7d40
Nhat Nam <phannam28082004@gmail.com>
2025-04-07 10:22:40 +0700

    day2

---
+++ Day2.py
@@ -0,0 +1,9 @@
+number = int(input("Mời nhập số:"))
+if number % 2 == 0:
+    message = "Đây là số chẵn"
+else:
+    message = "Đây là số lẻ"
+if number < 0:
+    message += " và là số âm"
+print(message)
+    
