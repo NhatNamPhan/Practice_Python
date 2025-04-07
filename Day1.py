04097543f1c94af3228ed3fe8c39a2e949a9b72c
Nhat Nam <phannam28082004@gmail.com>
2025-04-07 09:50:56 +0700

    first commit

---
+++ Day1.py
@@ -0,0 +1,4 @@
+name = input("Nhập tên của bạn:")
+old = int(input("Nhập tuổi của bạn:"))
+height = float(input("Nhập chiều cao của bạn:"))
+print(f"Tên tôi là {name}, tuổi {old}, chiều cao {height}")
