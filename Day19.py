56a5196fb000975431387747a1c3bc3366a87a7c
Nhat Nam <phannam28082004@gmail.com>
2025-04-29 22:11:21 +0700

    day19

---
+++ Day19.py
@@ -0,0 +1,58 @@
+class Product():
+    def __init__(self, product_id:int, name, price:float):
+        self.product_id = product_id
+        self.name = name
+        self.price = price
+class OrderItem():
+    def __init__(self, product:Product, quantity:int):
+        self.product = product
+        self.quantity = quantity
+    def get_total_price(self) -> float:
+        return self.product.price * self.quantity
+class Order():
+    def __init__(self,order_id:int, order_items: list[OrderItem] = None):
+        self.order_id = order_id
+        self.order_items = order_items if order_items is not None else []
+    def add_item(self,order_item):
+        self.order_items.append(order_item)
+    def get_total_order(self) -> float:
+        return sum(item.get_total_price() for item in self.order_items)
+    def display_order(self):
+        print(f"Đơn hàng: {self.order_id}")
+        print("-"*28)
+        for item in self.order_items:
+            print(f"Sản phẩm: {item.product.name}")
+            print(f"Số lượng: {item.quantity}")
+            print(f"Giá: {item.product.price}")
+            print(f"Thành tiền: {item.get_total_price()}")
+            print("-"*28)
+        print(f"Tổng đơn hàng: {self.get_total_order()}")
+def menu():
+    order = None
+    while True:
+        print('\n1. Đặt hàng')
+        print('2. Hóa đơn đặt hàng')
+        print('3. Thoát')
+        choose = int(input("Chọn chức năng:"))
+        if choose == 1:
+            if order is None:
+                order_id = int(input("Id đơn hàng:"))
+                order = Order(order_id,[])
+            product_id = int(input("Id sản phẩm:"))
+            name = input("Sản phẩm:")
+            price = float(input("Giá sản phẩm:"))
+            quantity = int(input("Số lượng:"))
+            product = Product(product_id,name,price)
+            order_item = OrderItem(product,quantity)
+            order.add_item(order_item)
+        elif choose == 2:
+            if order is not None:
+                order.display_order()
+            else: print("Chưa có đơn hàng nào được tạo!!!")
+        elif choose == 3:
+            print("Tạm biệt!!!")
+            break
+        else: print("Lựa chọn không hợp lệ!!!")
+
+menu()
+            
